28ca76b44ebe5e71a133a586241c5c9cd1e739ac
Vasiliev Evgenij <vasiliev.evgenij@gmail.com>
2018-10-08 22:36:27 +0300

    all works

--- src/parserHTML/Main.java
+++ src/parserHTML/Main.java
@@ -5,22 +5,48 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+                                        DataSourceTransactionManagerAutoConfiguration.class,
+                                            HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class Main extends Application {
 
+    private ConfigurableApplicationContext context;
+    //private SpringApplicationCon context;
+    private Parent rootNode;
+
+    @Override
+    public void init() throws Exception {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
+        context = builder.run();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/Home.fxml"));
+        loader.setControllerFactory(context::getBean);
+        rootNode = loader.load();
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Home.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("fxml/Home.fxml"));
         primaryStage.setTitle("HTML PARSER");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(rootNode));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop() throws Exception {
+        context.close();
     }
+
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
